d34c4bcb212f29a2762711dd40b2af1a9411e6aa
Dan Thompson <thompsondanielk@gmail.com>
2023-10-14 00:49:59 -0400

    initial commit

---
+++ Engine/Interfaces/ICharacter.cs
@@ -0,0 +1,33 @@
+﻿using UntitledRPG.Engine.Models;
+
+namespace UntitledRPG.Engine.Interfaces
+{
+    public interface ICharacter
+    {
+        int Charisma { get; set; }
+        string Class { get; set; }
+        int Constitution { get; set; }
+        int CurrentHealth { get; set; }
+        int Dexterity { get; set; }
+        int ExperiencePoints { get; set; }
+        int Gold { get; set; }
+        int Initiative { get; }
+        int Intelligence { get; set; }
+        Inventory Inventory { get; set; }
+        int Level { get; set; }
+        int MaxHealth { get; set; }
+        string Name { get; set; }
+        string Race { get; set; }
+        int Strength { get; set; }
+        int Wisdom { get; set; }
+
+        void Attack(Character target);
+        void CastSpell(string spellName, Character target);
+        int GetAbilityModifier(int abilityScore);
+        void Heal(int healingAmount);
+        bool IsDefeated();
+        void LevelUp();
+        int RollInitiative();
+        void TakeDamage(int damage);
+    }
+}
