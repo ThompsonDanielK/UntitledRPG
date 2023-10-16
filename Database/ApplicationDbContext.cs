64b6dc01d988646e68649ea3a3d978fe67f980a5
Dan Thompson <thompsondanielk@gmail.com>
2023-10-16 15:32:39 -0400

    Laying groundwork to implement MySql

---
+++ Database/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UntitledRPG.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseMySql( "Server=your_mysql_server;Database=your_database_name;User=your_username;Password=your_password;");
+            }
+        }
+    }
+}
